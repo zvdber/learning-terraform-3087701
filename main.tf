f98d5721e3dc9c732fb9dbc2f73bb30b606aa653
zvdber <70197248+zvdber@users.noreply.github.com>
2023-05-31 21:14:13 +0400

    vpc

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ module "blog_sg" {
   version = "4.17.2"
   name = "blog"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.dev.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
