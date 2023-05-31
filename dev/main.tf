8a3b7f458f85a0744c7a159eba5d433e09500f7d
zvdber <70197248+zvdber@users.noreply.github.com>
2023-06-01 02:31:31 +0400

    Define QA Env

--- dev/main.tf
+++ dev/main.tf
@@ -1,10 +1,3 @@
-module "qa" {
+module "dev" {
     source = "../modules/blog"
-
-    environment = {
-        name            = "qa"
-        network_prefix  = "10.1"
-    }
-    asg_min_size = 1
-    asg_max_size = 1
 }
