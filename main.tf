ee78241fe0a5ef5aee63656af66d2ccbc34e53ce
zvdber <70197248+zvdber@users.noreply.github.com>
2023-06-01 00:22:17 +0400

    false

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "blog_vpc" {
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway = false
 
   tags = {
     Terraform = "true"
