5a1e64cb192b31eb6700004d23ebbeb9165d0221
zvdber <70197248+zvdber@users.noreply.github.com>
2023-06-01 00:09:45 +0400

    3_7

--- main.tf
+++ main.tf
@@ -14,27 +14,13 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+data "aws_vpc" "default" {
+  default = true
 }
 
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -42,12 +28,11 @@ resource "aws_instance" "blog" {
   }
 }
 
-
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.blog_vpc.vpc_id
+  vpc_id  = data.aws_vpc.default.id
   name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
